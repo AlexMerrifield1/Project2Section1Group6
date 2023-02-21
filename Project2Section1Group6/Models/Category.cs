175537cdbf306245bf6cb8acf7cd081750ce8fa3
Alex Merrifield <alex.merrifield13@gmail.com>
2023-02-21 11:36:02 -0800

    Added Task and Category Model along with a TaskContext Model with some seeded data for Categories and a Task. I also downloaded some framework packages.

---
+++ Project2Section1Group6/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2Section1Group6.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int CategoryID { get; set; }
+
+        public string CategoryType { get; set; }
+    }
+}
