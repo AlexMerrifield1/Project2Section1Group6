6f10067d3ca68e932c01dec6e85c10cc11c5040a
Alex Merrifield <alex.merrifield13@gmail.com>
2023-02-21 11:06:28 -0800

    Add project files.

---
+++ Project2Section1Group6/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project2Section1Group6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
