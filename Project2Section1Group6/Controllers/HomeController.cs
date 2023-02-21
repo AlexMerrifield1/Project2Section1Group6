1dd4f67cbc70efdd870c9d566df2700eb992f205
DsrKrasniii <donredd3rd@gmail.com>
2023-02-21 11:56:17 -0700

    Controller Almost set up, just waiting to get more variable names

--- Project2Section1Group6/Controllers/HomeController.cs
+++ Project2Section1Group6/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Project2Section1Group6.Models;
 using System;
@@ -12,21 +13,87 @@ namespace Project2Section1Group6.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        // Initialize instance of object
+        private TaskContext myContext { get; set; }
+                                                             // Pass In data from context file
+        public HomeController(ILogger<HomeController> logger, TaskContext pointlessName)
         {
             _logger = logger;
-        }
 
+            // Lock-in instance of object
+            myContext = pointlessName;
+        }
+        // Index (HomePage) Direct
         public IActionResult Index()
         {
             return View();
         }
-
+        // Privacy Direct
         public IActionResult Privacy()
         {
             return View();
         }
+
+        //Create Task Page Direct
+        [HttpGet]
+        public IActionResult CreateTasks()
+        {
+            return View();
+        }
+        //Return data for database from Create Task Page
+        [HttpPost]
+        public IActionResult CreateTasks()
+        {
+            return View();
+        }
+
+        //Quadrant Page Direct
+        [HttpGet]
+        public IActionResult Quadrants()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int localID) // Edit where where the ID is 
+        {
+            ViewBag.Cat = myContext.Categories.ToList();
+
+            var application = myContext.Responses.Single(x => x.ForeignID == localID);
+
+            return View("CreateTask", application);
+        }
+        [HttpPost]
+        public IActionResult Edit(Task t) // Change Database values
+        {
+            myContext.Update(t);
+            myContext.SaveChanges();
+
+            return RedirectToAction("Quadrants");
+        }
+        [HttpGet]
+        public IActionResult Delete(int localID) // remove data entry where ID is
+        {
+            var application = myContext.Responses.Single(x => x.ForeignID == localID);
+            return View(application);
+        }
+        [HttpPost]
+        public IActionResult Delete(Task t) // remove data entry where Movie ID is
+        {
+            myContext.Responses.Remove(t);
+            myContext.SaveChanges();
+
+
+            return RedirectToAction("Quadrants");
+        }
+
+
+
+
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
