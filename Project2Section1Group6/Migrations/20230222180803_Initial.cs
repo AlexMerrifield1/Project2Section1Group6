8a43a193697abdbf5eb2ecb4551c2b60198881a5
Alex Merrifield <alex.merrifield13@gmail.com>
2023-02-22 10:08:23 -0800

    migrations added

---
+++ Project2Section1Group6/Migrations/20230222180803_Initial.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project2Section1Group6.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryType = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    TaskID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryID = table.Column<int>(nullable: false),
+                    TaskTitle = table.Column<string>(nullable: false),
+                    QuadrantNumber = table.Column<int>(nullable: false),
+                    DueDate = table.Column<string>(nullable: true),
+                    TaskNotes = table.Column<string>(maxLength: 200, nullable: true),
+                    Completed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.TaskID);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryType" },
+                values: new object[] { 1, "Home" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryType" },
+                values: new object[] { 2, "School" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryType" },
+                values: new object[] { 3, "Work" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryID", "CategoryType" },
+                values: new object[] { 4, "Church" });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "TaskID", "CategoryID", "Completed", "DueDate", "QuadrantNumber", "TaskNotes", "TaskTitle" },
+                values: new object[] { 1, 1, false, "2023-08-06", 1, "", "Do your chores" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CategoryID",
+                table: "Tasks",
+                column: "CategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
